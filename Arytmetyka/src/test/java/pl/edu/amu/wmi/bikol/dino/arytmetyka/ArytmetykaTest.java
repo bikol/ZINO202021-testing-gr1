5dbdf0b1cf99a9d40b243fcc6b007b884d0c43b0
Nikodem Domaracki <56053287+Nikkoro@users.noreply.github.com>
2021-02-13 18:43:54 +0100

    Update ArytmetykaTest.java

--- Arytmetyka/src/test/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/ArytmetykaTest.java
+++ Arytmetyka/src/test/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/ArytmetykaTest.java
@@ -147,13 +147,15 @@ public class ArytmetykaTest {
         String result = instance.potegowanie(a, b);
         assertEquals(expResult, result);
     }
-  
-        @Test
+	
+	@Test
     public void testPotegowanie13() {
         System.out.println("potegowanie13");
-        String a = "five";
-        String b = "six";
+        String a = "4/2";
+        String b = "2";
         Arytmetyka instance = new Arytmetyka();
-        String expResult = "15625";
+        String expResult = "4";
+        String result = instance.potegowanie(a, b);
+        assertEquals(expResult, result);
     }
 }
